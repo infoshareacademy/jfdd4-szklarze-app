b90392cfdef0f5a39f33ad41b1665753038d6911
Sebastian Kreft <seba.kreft@gmail.com>
2016-10-12 22:50:05 +0200

    FDSJ-84 Products data added to project

---
+++ src/data/products.js
@@ -0,0 +1,128 @@
+export default products = [
+    {
+        productName: "Kajzerka",
+        price: 0.4,
+        category: "Pieczywo",
+        favorite: true
+    },
+    {
+        productName: "Bułka wiejska",
+        price: 0.6,
+        category: "Pieczywo",
+        favorite: false
+    },
+    {
+        productName: "Chleb oliwki",
+        price: 2.0,
+        category: "Pieczywo",
+        favorite: false
+    },
+    {
+        productName: "Chlep żytni",
+        price: 0.4,
+        category: "Pieczywo",
+        favorite: false
+    },
+    {
+        productName: "Coca-cola",
+        price: 2.99,
+        category: "Napoje",
+        favorite: true
+    },
+    {
+        productName: "Woda mineralna",
+        price: 1.6,
+        category: "Napoje",
+        favorite: false
+    },
+    {
+        productName: "Piwo Specjal",
+        price: 2.50,
+        category: "Napoje",
+        favorite: true
+    },
+    {
+        productName: "Sol pomarańczowy",
+        price: 2.99,
+        category: "Napoje",
+        favorite: false
+    },
+    {
+        productName: "Czekolada",
+        price: 2.99,
+        category: "Słodycze",
+        favorite: false
+    },
+    {
+        productName: "Drożdzówka",
+        price: 1.30,
+        category: "Słodycze",
+        favorite: false
+    },
+    {
+        productName: "Budyń",
+        price: 2.0,
+        category: "Słodycze",
+        favorite: false
+    },
+    {
+        productName: "Wafelek",
+        price: 1.20,
+        category: "Słodycze",
+        favorite: false
+    },
+    {
+        productName: "Cebula 1kg",
+        price: 1.00,
+        category: "Warzywa",
+        favorite: true
+    },
+    {
+        productName: "Burak 1kg",
+        price: 0.90,
+        category: "Warzywa",
+        favorite: false
+    },
+    {
+        productName: "Marchew 1kg",
+        price: 1.30,
+        category: "Warzywa",
+        favorite: false
+    },
+    {
+        productName: "Pendrive",
+        price: 20.0,
+        category: "Elektronika",
+        favorite: true
+    },
+    {
+        productName: "Płyta CD",
+        price: 3.0,
+        category: "Elektronika",
+        favorite: false
+    },
+    {
+        productName: "Akumulatory AAA",
+        price: 9.50,
+        category: "Elektronika",
+        favorite: false
+    },
+    {
+        productName: "Wiadro",
+        price: 19.50,
+        category: "Artykuły budowlane",
+        favorite: false
+    },
+    {
+        productName: "Cement 25kg",
+        price: 23.00,
+        category: "Artykuły budowlane",
+        favorite: false
+    },
+    {
+        productName: "Cegła",
+        price: 11.0,
+        category: "Artykuły budowlane",
+        favorite: false
+    }
+]
