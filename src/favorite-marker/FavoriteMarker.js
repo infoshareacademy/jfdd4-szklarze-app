db43c164ad5104f89fb52431034b54bf44bfc31d
Sebastian Kreft <seba.kreft@gmail.com>
2016-10-16 00:06:20 +0200

    FDSJ-79 FavoriteMarker adds products ids to favorites array

--- src/favorite-marker/FavoriteMarker.js
+++ src/favorite-marker/FavoriteMarker.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
+var favorites = [];
+
 export default class FavoriteMarker extends React.Component {
+
+    constructor() {
+        super()
+
+        this.markAsFavorite = this.markAsFavorite.bind(this)
+    }
+
+    markAsFavorite() {
+        favorites.push(this.props.productId);
+        console.log(favorites)
+    }
 
     render () {
         return (
-
+            <button onClick={this.markAsFavorite}>Dodaj do ulubionych</button>
         )
     }
 }
