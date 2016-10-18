30ef9d5da9c815c133e4a41242d1fd7a5e3a00e5
Sebastian Kreft <seba.kreft@gmail.com>
2016-10-18 16:22:20 +0200

    Products data is now accsessible from store

---
+++ src/all-products/reducer.js
@@ -0,0 +1,4 @@
+import products from '../data/products'
+
+
+export default (state = products, action) => state
--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { combineReducers, createStore, compose } from 'redux'
-
+import productsReducer from './all-products/reducer'
 
 let reducer = combineReducers({
-
+    products: productsReducer
 });
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
