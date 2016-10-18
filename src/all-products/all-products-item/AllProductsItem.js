66054c7a63a56f95d6a5bb8c0e3383bb3f50ec0d
Natalia Vetter <nataliavetter@gmail.com>
2016-10-18 15:38:20 +0200

    FDSJ-78 Checkpoint commit

--- src/all-products/all-products-item/AllProductsItem.js
+++ src/all-products/all-products-item/AllProductsItem.js
@@ -5,21 +5,24 @@ import FavoriteMarker from '../../favorite-marker/FavoriteMarker'
 
 
 
+
 export default (props) => (
     <Row>
         <div>
             <Col md={8}>
                 <ul>
                     <li> {props.productName} </li>
-                    <li> {props.price} </li>
+                    <li> {props.price} zł</li>
                     <li> {props.category} </li>
                 </ul>
             </Col>
-            <Col md={4}>
+            <Col md={2}>
                 <div>
                     <Counter />
                 </div>
-                <div>
+            </Col>
+            <Col md={2}>
+                <div className="favorite_marker">
                     <FavoriteMarker productId={props.productId} />
                 </div>
             </Col>
