f87e9218e35197ed36689062ac126adc9b1f8800
Sebastian Kreft <seba.kreft@gmail.com>
2016-10-19 22:16:52 +0200

    Products data is now accessible from state

--- src/all-products/AllProducts.js
+++ src/all-products/AllProducts.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import products from '../data/products'
+import products from '../data/products/products'
 import Filters from '../filters/Filters'
 import './AllProducts.css'
 
--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
 import { combineReducers, createStore} from 'redux'
-import productsReducer from './all-products/reducer'
 import filterReducer from  './filters/reducer'
 import allProductsReducer from './all-products/reducer'
+import productsReducer from './data/products/reducer'
+
 
 let reducer = combineReducers({
     products: productsReducer,
-    filters: filterReducer
+    filters: filterReducer,
     allProducts: allProductsReducer
 });
 
