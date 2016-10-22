10b5393b7e9bcc4955aa2b78695114bd7c25f054
natvet <nataliavetter@gmail.com>
2016-10-22 19:20:31 +0200

    styling/buttons properly linked

--- src/introduction/Introduction.js
+++ src/introduction/Introduction.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import {Jumbotron} from 'react-bootstrap'
+import { Link } from 'react-router'
 
 export default (props) =>
     <Jumbotron className="intro">
         <div className="intro-logo"></div>
         <h1>Janusz wie lepiej, zawsze.</h1>
         <ul className="intro-caption">
-            <p>W zakładce <a className="blue-button" href="/all-products">Stwórz listę</a> wybierz jakie produkty chcesz kupić</p>
-            <p>W zakładce <a className="blue-button" href="/shopping-lists">Twoje listy zakupów</a> obejrzyj swoje listy</p>
+            <p>W zakładce
+                <Link to={`/all-products`}><div className="blue-button">Stwórz listę</div></Link>
+                wybierz jakie produkty chcesz kupić</p>
+            <p>W zakładce
+                <Link to={`/shopping-lists`}><div className="blue-button">Twoje listy zakupów</div></Link>
+                obejrzyj swoje listy</p>
             <p>Wyświetl listę na telefonie i uszaj do sklepu!</p>
         </ul>
     </Jumbotron>
