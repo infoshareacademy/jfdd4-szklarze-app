1dff746e67952c1d1da59501236b9b497a4a0b08
Sebastian Kreft <seba.kreft@gmail.com>
2016-10-22 18:50:36 +0200

    FDSJ-90 Checkpoint commit - changes after merging

--- src/list-creator/ListCreator.js
+++ src/list-creator/ListCreator.js
@@ -3,7 +3,7 @@ import { saveNewList, setCurrentListName } from './actionCreators'
 import { connect } from 'react-redux'
 
 const mapStateToProps = (state) => ({
-
+    itemsToBuy: state.allProducts.itemsToBuy
 })
 
 const mapDispatchToProps = (dispatch) => ({
@@ -13,11 +13,15 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 const ListCreator = ({
-    saveNewList
+    saveNewList,
+    itemsToBuy
 }) => (
     <div>
         <input defaultValue={1} placeholder="Wpisz nazwę listy" onChange={(event) => console.log(event.target.value)}/>
-        <button onClick={() => saveNewList()}>
+        <button onClick={() =>
+            itemsToBuy.length === 0 ?
+                alert('Wybierz produkt, aby stworzyć listę') :
+                saveNewList()}>
             Stwórz nową listę
         </button>
     </div>
