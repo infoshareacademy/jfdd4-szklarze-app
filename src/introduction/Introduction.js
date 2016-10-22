b314d8824a7b498d87663c8175b226f951ed6beb
natvet <nataliavetter@gmail.com>
2016-10-22 18:51:30 +0200

    styling/checkpoint

--- src/introduction/Introduction.js
+++ src/introduction/Introduction.js
@@ -6,8 +6,8 @@ export default (props) =>
         <div className="intro-logo"></div>
         <h1>Janusz wie lepiej, zawsze.</h1>
         <ul className="intro-caption">
-            <p>W zakładce <button className="blue-button">Stwórz listę</button> wybierz jakie produkty chcesz kupić</p>
-            <p>W zakładce <button className="blue-button">Twoje listy zakupów</button> obejrzyj swoje listy</p>
+            <p>W zakładce <a className="blue-button" href="/all-products">Stwórz listę</a> wybierz jakie produkty chcesz kupić</p>
+            <p>W zakładce <a className="blue-button" href="/shopping-lists">Twoje listy zakupów</a> obejrzyj swoje listy</p>
             <p>Wyświetl listę na telefonie i uszaj do sklepu!</p>
         </ul>
     </Jumbotron>
