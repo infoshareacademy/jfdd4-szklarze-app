c69d956183ec32841413d1ff119525e5305c1acd
Sebastian Kreft <seba.kreft@gmail.com>
2016-10-23 18:23:54 +0200

    FDSJ-96 User is now redirected to shopping lists after creating new list

--- src/list-creator/ListCreator.js
+++ src/list-creator/ListCreator.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { saveNewList, setCurrentListName } from './actionCreators'
 import { connect } from 'react-redux'
+import { MenuItem } from 'react-bootstrap'
+import {browserHistory} from 'react-router'
 
 const mapStateToProps = (state) => ({
     itemsToBuy: state.allProducts.itemsToBuy,
@@ -10,30 +12,42 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
     saveNewList: () => dispatch(saveNewList()),
     setCurrentListName: (listName) => dispatch(setCurrentListName(listName))
-
 })
 
-const ListCreator = ({
-    saveNewList,
-    setCurrentListName,
-    itemsToBuy,
-    currentListName
-}) => (
-    <div>
-        <input
-            onChange={(event) => setCurrentListName(event.target.value)}
-            value={currentListName}
-            placeholder="Wpisz nazwę listy..."
-        />
-        <button onClick={() =>
-            itemsToBuy.length === 0 ?
-                alert('Wybierz produkt, aby stworzyć listę') :
-                saveNewList()}>
-            Stwórz nową listę
-        </button>
+class ListCreator extends React.Component {
 
-    </div>
-)
+    handleSelect(eventKey) {
+        event.preventDefault();
+        browserHistory.push(eventKey);
+    }
 
+    render() {
+        const {
+            saveNewList,
+            setCurrentListName,
+            itemsToBuy,
+            currentListName
+        } = this.props
+        return (
+            <div>
+                <input
+                    onChange={(event) => setCurrentListName(event.target.value)}
+                    value={currentListName}
+                    placeholder="Wpisz nazwę listy..."
+                />
+                <MenuItem
+                    bsClass="create-new-list-button"
+                    onClick={() =>
+                        itemsToBuy.length === 0 ?
+                            alert('Wybierz produkt, aby stworzyć listę') :
+                            saveNewList()}
+                    eventKey="/shopping-lists"
+                    onSelect={this.handleSelect}>
+                    Stwórz nową listę
+                </MenuItem>
+            </div>
+        )
+    }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(ListCreator)
