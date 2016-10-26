1ef6fbba787668754040f53e69e293316ce25174
Natalia Vetter <nataliavetter@gmail.com>
2016-10-26 23:06:52 +0200

    FDSJ-110 map is device width and height now

--- src/map/Map.js
+++ src/map/Map.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import GoogleMap from 'google-map-react'
 import Place from './place/Place'
+import './Map.css'
 
 export default (props) =>
-    <div style={{width: '300px', height: '300px'}}>
+    <div className="map-container">
         <GoogleMap
             center={[59.938043, 30.337157]}
             zoom={9}>
