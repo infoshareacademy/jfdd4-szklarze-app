6ea3c1ba830ce8af19725cacac6d6d1a532b0fe8
Natalia Vetter <nataliavetter@gmail.com>
2016-10-26 23:00:14 +0200

    FDSJ-110 google-map-react installed, simple map is being displayed

--- src/map/Map.js
+++ src/map/Map.js
@@ -1,1 +1,13 @@
 import React from 'react'
+import GoogleMap from 'google-map-react'
+import Place from './place/Place'
+
+export default (props) =>
+    <div style={{width: '300px', height: '300px'}}>
+        <GoogleMap
+            center={[59.938043, 30.337157]}
+            zoom={9}>
+            <Place lat={59.955413} lng={30.337844} text={'A'}/>
+            <Place lat={60.955420} lng={30.337860} text={'B'}/>
+        </GoogleMap>
+    </div>
---
+++ src/map/place/Place.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+export default (props) =>
+    <div style={{fontSize: '20px'}}>{props.text}</div>
