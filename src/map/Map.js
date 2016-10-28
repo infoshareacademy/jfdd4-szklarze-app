511c4fa01b027bda56fbeed137fd7fa6a0088d90
Natalia Vetter <nataliavetter@gmail.com>
2016-10-28 15:22:54 +0200

    FDSJ-110 checkpoint commit

--- src/map/Map.js
+++ src/map/Map.js
@@ -3,11 +3,30 @@ import GoogleMap from 'google-map-react'
 import Place from './place/Place'
 import './Map.css'
 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude);
+}
+
+getLocation();
+
 export default (props) =>
     <div className="map-container">
         <GoogleMap
+            bootstrapURLKeys={{
+                key: 'AIzaSyCmKq7BbW9E8wkMALYHdjMHo6D839_cstk'
+            }}
             center={[54.4416, 18.5601]}
-            zoom={9}>
+            zoom={9}
+            yesIWantToUseGoogleMapApiInternals
+            onClick={console.log}>
             <Place lat={54.4416} lng={18.5601} text={'A'}/>
         </GoogleMap>
     </div>
