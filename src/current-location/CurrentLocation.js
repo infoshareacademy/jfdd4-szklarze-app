a2289347aa9b95e054bcd3778688d903442aef29
Natalia Vetter <nataliavetter@gmail.com>
2016-11-01 23:05:57 +0100

    FDSJ-115 checkpoint

--- src/current-location/CurrentLocation.js
+++ src/current-location/CurrentLocation.js
@@ -2,41 +2,39 @@ import React from 'react'
 import GoogleMap from 'google-map-react'
 import Place from './place/Place'
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(currentPosition);
-    } else {
-        console.log("Geolocation is not supported by this browser.");
+
+const CurrentLocation = (props) => {
+
+    let bla = {};
+
+    function getLocation() {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log("Latitude: " + position.coords.latitude +
+            "<br>Longitude: " + position.coords.longitude);
+            bla.lat = position.coords.latitude;
+            bla.lng = position.coords.longitude;
+        })
     }
-}
-const currentPosition = function(position) {
-    console.log('elo')
-    return {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-    }
 
-}
+    getLocation();
 
-getLocation();
+    console.log(bla);
 
-
-export default class CurrentLocation extends React.Component {
-
-
-    render() {
         return (
-            <div style={{width: '300px', height: '300px'}} >
+            <div style={{width: '300px', height: '300px'}}>
+                {console.log(bla)}
                 <GoogleMap
                     bootstrapURLKeys={{
                         key: 'AIzaSyCmKq7BbW9E8wkMALYHdjMHo6D839_cstk'
                     }}
-                    center={[currentPosition.lat, currentPosition.lng]}
-                    zoom={6}>
-                    <Place lat={currentPosition.lat} lng={currentPosition.lng} label={'AMMMM'}/>
+                    center={[bla.lat, bla.lng]}
+                    zoom={6}
+                    yesIWantToUseGoogleMapApiInternals>
+                    <Place lat={bla.lat} lng={bla.lng} label={'AMMMM'}/>
                 </GoogleMap>
             </div>
         )
-    }
 }
+
+export default CurrentLocation
 
