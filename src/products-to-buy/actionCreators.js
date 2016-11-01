6eb74e433a643b980e5c7af8439613efeaef4dce
marcin-andrzejewski <marcin@andrzejewski.me>
2016-11-01 19:50:43 +0100

    add function fetchPrices

--- src/products-to-buy/actionCreators.js
+++ src/products-to-buy/actionCreators.js
@@ -1,5 +1,6 @@
 import {
-    MARK_PRODUCT_AS_PURCHASED
+    MARK_PRODUCT_AS_PURCHASED,
+    RECEIVE_PRICES
 } from './actionTypes'
 
 export function markProductAsPurchased(productId, listId) {
@@ -7,5 +8,20 @@ export function markProductAsPurchased(productId, listId) {
         type: MARK_PRODUCT_AS_PURCHASED,
         productId: productId,
         listId: listId
+    }
+}
+
+import { REQUEST_NOTES, RECEIVE_NOTES } from './actionTypes'
+
+import fetch from 'isomorphic-fetch'
+
+export function fetchPrices(productId) {
+    return function (dispatch) {
+        return fetch('https://jfdd4-szklarze-app-janusz.herokuapp.com/api/price-markers/?filter[where][productId]=' + productId +'&filter[fields][price]=true')
+            .then(response => response.json())
+            .then(prices => dispatch({
+                type: RECEIVE_PRICES,
+                prices: prices
+            }))
     }
 }
