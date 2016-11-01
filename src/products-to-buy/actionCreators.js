3d8fd230da6a19c3147fa2e3996e0ec5ec947738
marcin-andrzejewski <marcin@andrzejewski.me>
2016-11-01 13:19:57 +0100

    add 'listId' as payload to actionCreators (products-to-buy)

--- src/products-to-buy/actionCreators.js
+++ src/products-to-buy/actionCreators.js
@@ -3,10 +3,11 @@ import {
     RESET_PURCHASED
 } from './actionTypes'
 
-export function markProductAsPurchased(productId) {
+export function markProductAsPurchased(productId, listId) {
     return{
         type: MARK_PRODUCT_AS_PURCHASED,
         productId: productId,
+        listId: listId
     }
 }
 
