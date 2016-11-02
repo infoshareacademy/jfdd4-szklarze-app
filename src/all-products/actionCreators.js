1815f434ca3abb1e28f21135b506af5103b70008
Sebastian Kreft <seba.kreft@gmail.com>
2016-11-02 20:02:30 +0100

    FDSJ-123 Added temporary funcionality for generating default price-markers

--- src/all-products/actionCreators.js
+++ src/all-products/actionCreators.js
@@ -26,24 +26,29 @@ export function addPriceMarker(price, productId){
         return Math.random() * (max - min) + min;
     }
 
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
     let minPrice = 0.8 * price,
         maxPrice = 1.2 * price;
 
-    return fetch('https://jfdd4-szklarze-app-janusz.herokuapp.com/api/users', {
-            method: 'post',
+    return fetch('http://0.0.0.0:3010/api/price-markers', {
+            method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
                 "productId": productId,
-                "price": getRandomArbitrary(minPrice,maxPrice),
+                "price": getRandomArbitrary(minPrice,maxPrice).toFixed(2),
                 "shopPosition": {
-                    "lat": getRandomArbitrary(54.313972, 54.382409),
-                    "lng": getRandomArbitrary(18.576980, 18.707786)
+                    "lat": getRandomArbitrary(54.313972, 54.382409).toFixed(6),
+                    "lng": getRandomArbitrary(18.576980, 18.707786).toFixed(6)
                 },
-                "date": "2016-10-29T14:25:36.000Z",
-                "userId": 5,
+                "date": "2016-10-"+
+                getRandomInt(1, 31)+"T"+getRandomInt(6, 22)+":"+getRandomInt(1, 59)+":00.000Z",
+                "userId": getRandomInt(1, 5),
             })
-        })
+        }).then(response => addPriceMarker(price, productId))
 }
