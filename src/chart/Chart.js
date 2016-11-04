cd2b8ade85e2090645ad4f77396cbe9d552aa3fb
marcin-andrzejewski <marcin@andrzejewski.me>
2016-11-04 14:52:15 +0100

    disable animation - fix bug

--- src/chart/Chart.js
+++ src/chart/Chart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Line} from 'react-chartjs-2';
+import {Line, defaults} from 'react-chartjs-2';
 import {connect} from 'react-redux'
 
 defaults.global.animation = false;
